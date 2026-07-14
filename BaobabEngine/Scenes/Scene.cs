4cada16bc08f035e6b2f70335563210d2cf1d882
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-07-14 16:58:36 -0500

    Create Scene.cs If you have any suggestions for improving this, PLEASE reach out.

---
+++ BaobabEngine/Scenes/Scene.cs
@@ -0,0 +1,18 @@
+﻿using Foster.Framework;
+using System;
+
+namespace BaobabEngine.Scenes;
+
+/// <summary>
+/// Used to create contained scenes with their own objects and behavior.
+/// </summary>
+public abstract class Scene: IDisposable
+{
+    public abstract void Initialize();
+
+    public abstract void Update();
+
+    public abstract void Render(in Batcher batcher, in Window window);
+
+    public abstract void Dispose();
+}
