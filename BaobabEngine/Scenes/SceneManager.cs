8052454b9873ecdf014a0dd217cae1385d0dd147
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-07-18 14:39:55 -0500

    Create SceneManager.cs

---
+++ BaobabEngine/Scenes/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using Foster.Framework;
+using System;
+
+namespace BaobabEngine.Scenes;
+
+public class SceneManager(Scene startingScene)
+{
+    public Type Type => _currentScene.GetType();
+
+    public bool IsDisposed => _currentScene.IsDisposed;
+    
+    private Scene _currentScene = startingScene;
+
+    public void Update(float deltaTime) => _currentScene.Update(deltaTime);
+    public void Render(in Window window) => _currentScene.Render(window);
+    public void Dispose() => _currentScene.Dispose();
+
+    public void SwitchScene(Scene newScene)
+    {
+        _currentScene.Dispose();
+        _currentScene = newScene;
+    }
+}
