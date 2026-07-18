02278e536d5496810c90c0580091e45e02fd7c9e
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-07-18 14:44:14 -0500

    Add a comment to the SwitchScene method in SceneManager.cs

--- BaobabEngine/Scenes/SceneManager.cs
+++ BaobabEngine/Scenes/SceneManager.cs
@@ -15,6 +15,10 @@ public class SceneManager(Scene startingScene)
     public void Render(in Window window) => _currentScene.Render(window);
     public void Dispose() => _currentScene.Dispose();
 
+    /// <summary>
+    /// Disposes of the current scene and switches to a new scene.
+    /// </summary>
+    /// <param name="newScene">The scene that you want to switch to.</param>
     public void SwitchScene(Scene newScene)
     {
         _currentScene.Dispose();
