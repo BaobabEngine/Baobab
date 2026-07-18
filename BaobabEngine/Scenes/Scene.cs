a433e9cbf5bd123f2b3282fdbab2b38909a4ba58
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-07-18 13:03:22 -0500

    Update Scene.cs Fixes issues regarding disposal safety

--- BaobabEngine/Scenes/Scene.cs
+++ BaobabEngine/Scenes/Scene.cs
@@ -5,14 +5,38 @@ namespace BaobabEngine.Scenes;
 
 /// <summary>
 /// Used to create contained scenes with their own objects and behavior.
+///
+/// Note:
+/// `Update` and `Render` are the proper methods to call when updating and rendering a scene. These enforce safety
+/// with potentially trying to use disposed information. However, custom logic for updating and rendering is implemented
+/// through `UpdateScene` and `RenderScene`.
 /// </summary>
 public abstract class Scene: IDisposable
 {
-    public abstract void Initialize();
+    public bool IsDisposed { get; private set; }
 
-    public abstract void Update();
+    public void Update(float deltaTime)
+    {
+        ObjectDisposedException.ThrowIf(IsDisposed, this);
+        UpdateScene(deltaTime);
+    }
 
-    public abstract void Render(in Batcher batcher, in Window window);
+    public void Render(in Window window)
+    {
+        ObjectDisposedException.ThrowIf(IsDisposed, this);
+        RenderScene(window);
+    }
 
-    public abstract void Dispose();
+    public void Dispose()
+    {
+        if (IsDisposed) return;
+        
+        DisposeScene();
+        IsDisposed = true;
+    }
+    
+    protected abstract void UpdateScene(float deltaTime);
+    protected abstract void RenderScene(in Window window);
+
+    protected virtual void DisposeScene() { }
 }
