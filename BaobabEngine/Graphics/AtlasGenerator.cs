676b9c024a31b1549d981c11e6abe4b9705e028c
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-09-19 15:12:01 -0500

    Remove Path.Combine blocks from manual asset moving blocks in the atlas generator

--- BaobabEngine/Graphics/AtlasGenerator.cs
+++ BaobabEngine/Graphics/AtlasGenerator.cs
@@ -44,8 +44,8 @@ public class AtlasGenerator : IDisposable
 
     // Make sure to never include the root folder when adding an asset.
     // The root folder for assets is always handled using ContentRoot. 
-    public void AddAsset(string assetPath) => _assets.Add(Path.Combine(ContentRoot, assetPath));
-    public void RemoveAsset(string assetPath) => _assets.Remove(Path.Combine(ContentRoot, assetPath));
+    public void AddAsset(string assetPath) => _assets.Add(assetPath);
+    public void RemoveAsset(string assetPath) => _assets.Remove(assetPath);
 
     public void AddAsset(string[] assetPaths)    
     {
